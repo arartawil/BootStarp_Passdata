409b0c974d0f7ac7c62b3f15b3e399708796d3de
Magic Systems <Magic Systems@DESKTOP-I6B8EKN>
2023-12-22 01:16:38 +0300

    Add project files.

---
+++ BootStarp_Passdata/Controllers/UserController.cs
@@ -0,0 +1,52 @@
+﻿using BootStarp_Passdata.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BootStarp_Passdata.Controllers
+{
+    public class UserController : Controller
+    {
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            Login login = new Login();
+            login.username= "admin";
+            login.password= "admin"; 
+
+            ViewBag.login = login;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(Login login)
+        {
+            ViewBag.login = login;
+
+            if (login.username.Equals("Arar") && login.password.Equals("123456"))
+            {
+                string result = "Done";
+                TempData["Msg"] = result;
+                ViewBag.login = login;
+
+                ViewData["login"] = login;
+            }
+            else
+            {
+                string result = "Check your Account";
+                TempData["Msg"] = result;
+                ViewData["login"] = login;
+
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+
+
+    }
+}
---
+++ BootStarp_Passdata/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BootStarp_Passdata.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
